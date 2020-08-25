aece001f8cb72d80fcd78e9941cfe930def224d1
HALv2 <charlotte1787@gmail.com>
2020-08-25 15:47:46 +0900

    initialize

---
+++ assign1-4/index.js
@@ -0,0 +1,30 @@
+let tasks = [
+  {description: "机を片付ける", category: "掃除"}, 
+  {description: "牛乳を買う", category: "買い物"},
+  {description: "散歩する", category: "運動"}
+];
+
+function show() {
+  console.log("========================", "\n", "現在持っているタスクの一覧", "\n", "========================");
+  tasks.forEach(function (task, index) {
+    console.log(`${index}:[内容]${task.description},[ジャンル]${task.category}`);
+  })
+}
+
+function add() {
+  let newItemDescription = prompt("タスクを入力してください");
+  let newItemCategory = prompt("ジャンルを入力してください");
+  if (newItemDescription && newItemCategory) {
+    alert("新しいタスクを追加しました。");
+    tasks.push({description: newItemDescription, category: newItemCategory});
+  }
+  show();
+}
+
+function action() {
+  let action = prompt("「確認,追加,削除,修了」の４つのいずれかを入力してください");
+}
+
+show();
+add();
+action(); 
