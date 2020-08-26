6090ba59978b7fc183dfee7c4e452fe198e5b87d
HALv2 <charlotte1787@gmail.com>
2020-08-26 20:17:31 +0900

    chage function names to more descriptive ones

--- assign1-4/index.js
+++ assign1-4/index.js
@@ -4,27 +4,27 @@ let tasks = [
   {description: "散歩する", category: "運動"}
 ];
 
-function show() {
+function showTasks() {
   console.log("========================", "\n", "現在持っているタスクの一覧", "\n", "========================");
   tasks.forEach(function (task, index) {
     console.log(`${index} : [内容]${task.description},[ジャンル]${task.category}`);
   })
 }
 
-function add() {
+function addTasksToList() {
   let newItemDescription = prompt("タスクを入力してください");
   let newItemCategory = prompt("ジャンルを入力してください");
   if (newItemDescription && newItemCategory) {
     alert("新しいタスクを追加しました。");
     tasks.push({description: newItemDescription, category: newItemCategory});
   }
-  show();
+  showTasks();
 }
 
-function action() {
+function selectAction() {
   let action = prompt("「確認,追加,削除,修了」の４つのいずれかを入力してください");
 }
 
-show();
-add();
-action(); 
+showTasks();
+addTasksToList();
+selectAction(); 
