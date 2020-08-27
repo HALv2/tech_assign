9c4e01fc0c82a4b72f3bd5800ab5d4eaf9aafffc
HALv2 <charlotte1787@gmail.com>
2020-08-28 08:03:13 +0900

    change variable names

--- assign2/script.js
+++ assign2/script.js
@@ -1,23 +1,25 @@
 function myCalc() {
-  const submitedValues = document.querySelector("#fizzBuzzForm").elements;
+  const rawValues = document.querySelector("#fizzBuzzForm").elements;
   
-  const rawFizz = submitedValues[0].value;
-  const rawBuzz = submitedValues[1].value;
+  const rawFizz = rawValues[0].value;
+  const rawBuzz = rawValues[1].value;
   
-  const fizzNumber = Number(rawFizz);
-  const buzzNumber = Number(rawBuzz);
+  const inputNumbers = { 
+    fizz: Number(rawFizz),
+    buzz: Number(rawBuzz),
+  }
   
   const checkIfInteger = (rawFizz && rawBuzz) &&  //check if input is empty.
-    Number.isInteger(fizzNumber) && Number.isInteger(buzzNumber); //check if input is integer.
+    Number.isInteger(inputNumbers.fizz) && Number.isInteger(inputNumbers.buzz); //check if input is integer.
 
   if (checkIfInteger) {
-    let i = Math.min(fizzNumber, buzzNumber);      
+    let i = Math.min(inputNumbers.fizz, inputNumbers.buzz);      
     for (i; i < 100; i++) {
-      if (i % fizzNumber === 0 && i % buzzNumber === 0) {
+      if (i % inputNumbers.fizz === 0 && i % inputNumbers.buzz === 0) {
         showResult("fizzAndBuzz", i);
-      } else if (i % fizzNumber === 0){
+      } else if (i % inputNumbers.fizz === 0){
         showResult("fizz", i);
-      } else if (i % buzzNumber === 0){
+      } else if (i % inputNumbers.buzz === 0){
         showResult("buzz", i);
       }
     }
