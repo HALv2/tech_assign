ad4131bf05a612cec689e334563d3ccae2acd639
HALv2 <charlotte1787@gmail.com>
2020-09-02 20:30:06 +0900

    reassign id to task

--- assign3-2/script.js
+++ assign3-2/script.js
@@ -1,5 +1,6 @@
 document.getElementById("submit").addEventListener("click", submitNewTask);
 
+
 function submitNewTask() {
   const inputTask = {
     id: parseInt(document.querySelector(".task_list").rows.length - 1),
@@ -8,10 +9,24 @@ function submitNewTask() {
     delete: "削除",
   }
   createNewTaskElement(inputTask);
+
+  const node = document.querySelectorAll(".delete");
+  for (let i = 0; i < node.length; i++){
+    node[i].addEventListener("click", function(){
+      event.target.parentNode.parentNode.remove();
+      
+      const tableLength = document.querySelector(".task_list").rows.length - 1;
+      for (let i = 0; i < tableLength; i++){
+        const taskIdNode = document.querySelectorAll(".task_id");
+        taskIdNode[i].innerHTML = i;
+      }
+    });
+  }
 }
 
-function createTextCell(parent, value) {
+function createTextCell(parent, value, className) {
   const childNode = document.createElement("td");
+  childNode.setAttribute("class", `${className}`);
   const cell = document.createTextNode(`${value}`);
   childNode.appendChild(cell);
   parent.appendChild(childNode);
@@ -19,11 +34,11 @@ function createTextCell(parent, value) {
 
 function createButtonCell(parent, v1, v2) {
   const childNode = document.createElement("td");
+  childNode.setAttribute("class", `${v1}`);
   const cell = document.createElement("input");
   Object.assign(cell, {
     type: "button",
-    id: `${v1}`,
-    value: `${v2}`,
+    value: `${v2}`
   });
   childNode.appendChild(cell);
   parent.appendChild(childNode);
@@ -32,8 +47,8 @@ function createButtonCell(parent, v1, v2) {
 function createNewTaskElement(object) {
   const parentNode = document.createElement("tr");
   
-  createTextCell(parentNode, object.id);
-  createTextCell(parentNode, object.comment);
+  createTextCell(parentNode, object.id, "task_id");
+  createTextCell(parentNode, object.comment, "comment");
   
   createButtonCell(parentNode, "status", object.status);
   createButtonCell(parentNode, "delete", object.delete);
