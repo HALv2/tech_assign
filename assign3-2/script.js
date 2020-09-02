9d73f6e2a1660091156c29915831c52c35d303e4
HALv2 <charlotte1787@gmail.com>
2020-09-02 18:50:55 +0900

    initialze

---
+++ assign3-2/script.js
@@ -0,0 +1,42 @@
+document.getElementById("submit").addEventListener("click", submitNewTask);
+
+function submitNewTask() {
+  const inputTask = {
+    id: parseInt(document.querySelector(".task_list").rows.length - 1),
+    comment: document.getElementById("comment").value,
+    status: "作業中",
+    delete: "削除",
+  }
+  createNewTaskElement(inputTask);
+}
+
+function createTextCell(parent, value) {
+  const childNode = document.createElement("td");
+  const cell = document.createTextNode(`${value}`);
+  childNode.appendChild(cell);
+  parent.appendChild(childNode);
+}
+
+function createButtonCell(parent, v1, v2) {
+  const childNode = document.createElement("td");
+  const cell = document.createElement("input");
+  Object.assign(cell, {
+    type: "button",
+    id: `${v1}`,
+    value: `${v2}`,
+  });
+  childNode.appendChild(cell);
+  parent.appendChild(childNode);
+}
+
+function createNewTaskElement(object) {
+  const parentNode = document.createElement("tr");
+  
+  createTextCell(parentNode, object.id);
+  createTextCell(parentNode, object.comment);
+  
+  createButtonCell(parentNode, "status", object.status);
+  createButtonCell(parentNode, "delete", object.delete);
+
+  document.querySelector(".task_list").appendChild(parentNode);
+}
