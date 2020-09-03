30e773b2ebbe14e185345d4d54ef42f3034e818c
HALv2 <charlotte1787@gmail.com>
2020-09-04 07:18:52 +0900

    change status

--- assign3-2/script.js
+++ assign3-2/script.js
@@ -1,27 +1,45 @@
 document.getElementById("submit").addEventListener("click", submitNewTask);
 
-
 function submitNewTask() {
+  let tasks = [];
   const inputTask = {
-    id: parseInt(document.querySelector(".task_list").rows.length - 1),
+    id: parseInt(document.getElementById("list").rows.length),
     comment: document.getElementById("comment").value,
     status: "作業中",
     delete: "削除",
-  }
-  createNewTaskElement(inputTask);
+  };
 
+  createNewTaskElement(inputTask);
+  tasks.push(inputTask);
+
+  
   const node = document.querySelectorAll(".delete");
-  for (let i = 0; i < node.length; i++){
+  for (let i = 0; i < node.length ; i++) {
     node[i].addEventListener("click", function(){
+      const deletedId = event.target.parentNode.parentNode.querySelector(".task_id").innerHTML;
       event.target.parentNode.parentNode.remove();
+      const nodeLength = document.getElementById("list").rows.length;
       
-      const tableLength = document.querySelector(".task_list").rows.length - 1;
-      for (let i = 0; i < tableLength; i++){
-        const taskIdNode = document.querySelectorAll(".task_id");
-        taskIdNode[i].innerHTML = i;
+      for ( let e = 0; e < nodeLength; e++) {
+        let taskIdNode = document.getElementById("list").rows[e].cells[0];
+        taskIdNode.innerHTML = e;
       }
     });
-  }
+  };
+
+  const editNode = document.querySelectorAll(".status");
+  for (let i = 0; i < node.length ; i++) {
+    node[i].addEventListener("click", function(){
+      const deletedId = event.target.parentNode.parentNode.querySelector(".task_id").innerHTML;
+      event.target.parentNode.parentNode.remove();
+      const nodeLength = document.getElementById("list").rows.length;
+      
+      for ( let e = 0; e < nodeLength; e++) {
+        let taskIdNode = document.getElementById("list").rows[e].cells[0];
+        taskIdNode.innerHTML = e;
+      }
+    });
+  };
 }
 
 function createTextCell(parent, value, className) {
@@ -32,13 +50,13 @@ function createTextCell(parent, value, className) {
   parent.appendChild(childNode);
 }
 
-function createButtonCell(parent, v1, v2) {
+function createButtonCell(parent, className, buttonValue) {
   const childNode = document.createElement("td");
-  childNode.setAttribute("class", `${v1}`);
+  childNode.setAttribute("class", `${className}`);
   const cell = document.createElement("input");
   Object.assign(cell, {
     type: "button",
-    value: `${v2}`
+    value: `${buttonValue}`
   });
   childNode.appendChild(cell);
   parent.appendChild(childNode);
@@ -53,5 +71,5 @@ function createNewTaskElement(object) {
   createButtonCell(parentNode, "status", object.status);
   createButtonCell(parentNode, "delete", object.delete);
 
-  document.querySelector(".task_list").appendChild(parentNode);
+  document.getElementById("list").appendChild(parentNode);
 }
